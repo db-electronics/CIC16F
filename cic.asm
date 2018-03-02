a0cf4ba339ccc7b5ee0f6e32abee986ae3a7d807
rene richard <rene@db-electronics.ca>
2018-03-02 15:50:24 -0500

    porting from 12F

--- cic.asm
+++ cic.asm
@@ -123,14 +123,172 @@ main
 	movwf	PIE0
 	banksel PORTA
 
+; 17 from POR to cycles to here
 ; timing critical section here,
 ; lock sends stream ID. 15 cycles per bit--------
 ; stream id read at 34th, 49th, 64th and 79th cycles
+
+; burn 17 cycles
+	movlw	LOW region
+	movwf	FSR1L
+	movlw	HIGH region
+	movwf	FSR1H
+	movlw	0x01		; wait = (3*W) + 5
+	call	wait		; burn 8 cycles
+	nop
+	nop
+	nop
+	nop
+
+	btfsc	PORTA, din	; check stream ID bit
+	bsf	0x31, 3		; copy to lock seed
+	movlw	0x02		; wait=3*W+5
+	call	wait		; burn 11 cycles
+	nop
+	nop
+
+	btfsc	PORTA, din	; check stream ID bit
+	bsf	0x31, 0		; copy to lock seed
+	movlw	0x02		;
+	call	wait		; burn 11 cycles
+	nop
+	nop
+
+	btfsc	PORTA, din	; check stream ID bit
+	bsf	0x31, 1		; copy to lock seed
+	movlw	0x02		;
+	call	wait		; burn 11 cycles
+	nop
+	nop
+
+	btfsc	PORTA, din	; check stream ID bit
+	bsf	0x31, 2		; copy to lock seed
+
+; 80 cycles to here
+; both seeds must be loaded within cycle 154
+; 154 - 80 = 74 cycles to load
+; curent region is stored in program flash, load and call proper loading subroutine
+	
+	moviw	0[FSR1]		; read lock/key offset for computed goto
+	
+; 85 cycles
+; 0x00 = 3193 - USA/Canada
+; 0x01 = 3195 - Europe
+; 0x02 = 3196 - Asia 
+; 0x03 = 3197 - UK/Italy/Australia
+	
+	brw			; add index in W to program counter
+	goto	load3193	; 2 + 60 - load USA/Canada seeds
+	goto	load3195	; 2 + 60 - load Europe seeds
+	goto	load3196	; 2 + 60 - load Asia seeds
+	goto	load3197	; 2 + 60 - load UK/Italy/Australia seeds
 	
 doneload	
-loop	
-	goto	loop
+	
+; 147 cycles
+;	clrf	FSR0H		; setup pointers
+	movlw	LOW lock	; FSR0 points to lock/key region
+	movwf	FSR0L
+	clrf	FSR1H
+	movlw	LOW x		; FSR1 points to X register emulator
+	movwf	FSR1L
+	banksel	PORTA		; just to be sure
+	
+;************************************************************************
+; 154 cycles to main loop
+	
+mainloop
+	movlw	0x01		; ldi 1
+mainloop28
+	movwf	x		; lxa - load x with a
+mainloop54
+	bcf	FSR0L, 4	; lbmi 0
+			    ; ** 156 - in sync
+	call	nextstreambit	; tml 147 - 10 cycles + 2 for call
+			    ; ** 168 - in sync
+	bsf	FSR0L, 4	; lbmi 1 - setting bit 4 changes 0x20 to 0x30
+	call	nextstreambit	; tml 147 - 10 cycles + 2 for call
+			    ; ** 181 - in sync
+	movlw	LOW key		; tml 174 ; H := 1 in key mode, 10 cycles + 2 for call
+	movwf	FSR0L
+			    ; ** 183 - ahead by 10 cycles
+	movlw	0x2		; burn 13 cycles
+	call	wait		; need to skip over 3 useless instructions here
+	nop			; tengen code tests din here (segher does not), maybe add this
+	nop
+			    ; ** 197 - in sync
+	movlw	INDF0		; ldi 0, x
+	clrf	INDF0		; ldi 0, x
+			    ; ** 199 - in sync
+	movwf	PORTA		; out	    // 200 ** key and lock both output here
+	movfw	PORTA		; in	    // 201 ** lock is nop here, to permit read
+	nop			; nop	    // 202 ** lock reads here
+	bcf	PORTA, dout	; out0	    // 203 ** clear output bit
+			    ; ** 203 - in sync
+	movwf	INDF0		; s - store input bit
+			    ; ** 204 - in sync
 
+			    
+; check if input bit matches what we output
+	; din = 0x30.1 (keyseed)
+	; calc = 0x20.0 (lockseed)
+	; if ( 0x20.0 != 0x30.1 ) { die() }
+	btfsc	key, 1		; if din == 0
+	goto	rcvdOne	
+rcvdZero
+	btfsc	lock, 0		; if calc == 0
+	goto	die		; din == 0, calc == 1 => die
+	goto	endCheckDin		; 
+rcvdOne
+	btfss	lock, 0		; if calc == 1
+	goto	die		; din == 1, calc == 0 => die
+	nop			; 6 cycles for comparison either way
+			    ; ** 210 - 20 cycles ahead
+endCheckDin
+		
+
+			    
+			    
+			    
+			    
+			    
+			    
+	goto	mainloop
+
+	
+;************************************************************************
+runhost
+;	BL = 1, outputs = 0 - 7 cycles (CIC takes 16 cycles)
+;************************************************************************			    
+	clrf	PORTA	    ; 1 - clear outputs
+	movlw	0xF0	    ; mask off nibble
+	andwf	FSR0L, f    ; apply to BL
+	movlw	0x01	    ; load BL with 1
+	iorwf	FSR0L, f	
+	return		    ; return, 7 cycles    
+	
+;************************************************************************
+nextstreambit
+;	tml 0x147
+;	[H:0] := NEXT STREAM BIT - 10 cycles either pass, CIC takes 10 cycles
+;************************************************************************
+	movfw	x		; xax - not really exchanging but it's overwritten right after
+	addwf	FSR0L		; add x (a really) 
+	btfss	INDF0,0		; l, ska 0 - skip if bit0 = 0 
+	goto	nsbskip		; t 11b
+	movlw	0xF0		; lbli 0
+	andwf	FSR0L		; lbli 0
+	movlw	0x05		; ldi 5
+	movwf	INDF0		; s
+	return
+nsbskip
+	movlw	0xF0		; lbli 0
+	andwf	FSR0L		; lbli 0
+	clrf	INDF0		; ldi 0, s
+	return
+
+	
+;************************************************************************	
 ; --------wait: 3*(W-1)+7 cycles (including call+return). W=0 -> 256!--------
 wait			    ; 2 for call
 	movwf	swait	    ; 1
@@ -138,6 +296,7 @@ wait0	decfsz	swait, f    ; 1 / 2 last pass
 	goto	wait0	    ; 2
 	return		    ; 2
 
+;************************************************************************
 ; --------wait long: 8+(3*(w-1))+(772*w). W=0 -> 256!--------
 longwait
 	movwf	lwait
@@ -147,8 +306,51 @@ longwait0
 	decfsz	lwait, f
 	goto	longwait0
 	return
+
 	
-; -----------------------------------------------------------------------
+;************************************************************************
+;-- change region in eeprom and die
+; 0x00 = 3193 - USA/Canada
+; 0x01 = 3195 - Europe
+; 0x02 = 3196 - Asia 
+; 0x03 = 3197 - UK/Italy/Australia
+die
+	; get current region byte
+	banksel NVMADRL		    
+	movlw	HIGH region
+	movwf	NVMADRH
+	movlw	LOW region
+	movwf	NVMADRL
+	bcf	NVMCON1, NVMREGS
+	bsf	NVMCON1, RD
+	movf	NVMDATL, W
+	movwf	INDF0		    ; store region in INDFO for now
+	
+	; erase row
+	bsf	NVMCON1, FREE
+	bsf	NVMCON1, WREN
+	bcf	INTCON, GIE
+	movlw	0x55
+	movwf	NVMCON2
+	movlw	0xAA
+	movwf	NVMCON2
+	bsf	NVMCON1, WR
+	bsf	INTCON, GIE
+	bcf	NVMCON1, WREN
+	
+	
+	
+; --------get caught up--------
+	banksel PORTA
+dietrap
+	bsf	PORTA, led	; LED on
+	nop
+	nop
+	bcf	PORTA, led	; LED on
+	goto	dietrap	
+	
+	
+;************************************************************************
 ; 3193 - USA/Canada 
 ; LOCK: 3952F20F9109997 - avrcic
 ; LOCK: $1952f8271981115 - segher
@@ -162,7 +364,7 @@ load3193
 	loadkey	    0x9,0x5,0x2,0x1,0x2,0x1,0x7,0x1,0x9,0x8,0x5,0x7,0x1,0x5
 	goto	doneload
 
-; -----------------------------------------------------------------------
+;************************************************************************
 ; 3195 - Europe 
 ; LOCK: $17BEF0AF5706617 
 ; LOAD LOCK SEED (30 cycles)
@@ -174,7 +376,7 @@ load3195
 	loadkey	    0x7,0xB,0xD,0x3,0x0,0x9,0xF,0x6,0xE,0xF,0x2,0xF,0x9,0x7	
 	goto	doneload
 
-; -----------------------------------------------------------------------
+;************************************************************************
 ; 3196 - Asia 
 ; LOCK: 06AD70AF6EF666C  
 ; LOAD LOCK SEED (30 cycles)
@@ -186,7 +388,7 @@ load3196
 	loadkey	    0x6,0xA,0xD,0xC,0xF,0x6,0x0,0x6,0xE,0xF,0x2,0xF,0x9,0x7	
 	goto	doneload
 	
-; -----------------------------------------------------------------------
+;************************************************************************
 ; 3197 - UK/Italy/Australia  
 ; LOCK: 558937A00E0D66D   
 ; LOAD LOCK SEED (30 cycles)
@@ -198,4 +400,8 @@ load3197
 	loadkey	    0x7,0x9,0xA,0xA,0x1,0xE,0x0,0xD,0x0,0x1,0x9,0xD,0x9,0x9	
 	goto	doneload
 
+;************************************************************************
+	org 0x780
+region	db	0x01, 0x01  ; default to NA region because we're the center of the world
+	
 end
